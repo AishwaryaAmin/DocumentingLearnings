c67b15e34e6a558a421f4f74756a0ed56a74a4e8
AishwaryaAmin <aishwaryaamin78661>
2026-01-30 13:13:10 -0300

    Add the customers

--- models/customer.js
+++ models/customer.js
@@ -1,36 +1,40 @@
-const { Schema } = mongoose;
-
+const mongoose = require("mongoose");
 
 main()
-    .then(() => console.log("connect successful"))
-    .catch((err) => console.log(err));
+  .then(() => console.log("connect successful"))
+  .catch((err) => console.log(err));
 
 async function main() {
-    mongoose.connect("mongodb://127.0.0.1:27017/test");
+  mongoose.connect("mongodb://127.0.0.1:27017/document");
 }
 
 const orderSchema = new Schema({
-    item: String,
-    price : Number
-})
+  item: String,
+  price: Number,
+});
 
 const customerSchema = new Schema({
-    name: String,
-    orders: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Order",
-        }
-    ]
-})
+  name: String,
+  orders: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
+});
 
-const order = mongoose.model("Order", orderSchema) 
+const order = mongoose.model("Order", orderSchema);
 const Customer = mongoose.model("Customer", customerSchema);
 
 //Functions
 
 const findCustomer = async () => {
-    let result = await Customer.find({}).populate("orders");
-    console.log(result[0]);
-    
-}
+  let result = await Customer.find({}).populate("orders");
+  console.log(result[0]);
+};
+//Add the customers
+const addCust = (async = () => {
+  let newCust = new Customer({
+    name: "Aish",
+  });
+});
